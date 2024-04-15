c78a1488d3f2326be4f55c544af4c077166452de
ameglebm <ameglevr@gmail.com>
2024-04-15 16:04:11 -0300

    sd4

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_K
 requisicao = requests.get(link)
 requisicao_dic = requisicao.json()
 descricao = requisicao_dic['weather'][0]['description']
-temperatura = ""
+temperatura = requisicao_dic['main']['temp'] -273.15
 print(descricao, temperatura)
