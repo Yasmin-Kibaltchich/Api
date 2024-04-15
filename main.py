aa72407a885d739591d855434c998baccdefa591
ameglebm <ameglevr@gmail.com>
2024-04-15 16:29:04 -0300

    asd

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests
 
 API_KEY = "d366c9479bcbd63c047d7e6b7b4b7233"
 cidade = 'rio de janeiro'
-link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}"
+link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&lang=pt_br"
 
 requisicao = requests.get(link)
 requisicao_dic = requisicao.json()
 descricao = requisicao_dic['weather'][0]['description']
 temperatura = requisicao_dic['main']['temp'] -273.15
-print(descricao, temperatura)
+print('{} {:.2f}°C'.format(descricao, temperatura))
